2b89c4596b8a7d3601dd39218d3ae626d3da362f
Pinki Parmar <parmarpinki932@gmail.com>
2023-06-19 15:36:16 +0530

    Done my work

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const { user } = require('../config');
+const { user, password } = require('../config');
 const con = require('../database');
 const { Encrypt } = require('../auth');
 const jwt = require('jsonwebtoken');
@@ -6,7 +6,8 @@ const secretkey = 'secretkeyFGFDGDFDGF';
 
 // function getUserList(req, res, next) {
 getUserList = function (req, res) {
-    let sql = "Select * from users";
+    const sql = "Select * from users";
+    
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Result: " + JSON.stringify(result));
@@ -15,7 +16,9 @@ getUserList = function (req, res) {
     }
 postRegister = async function (req, res) {
     // console.log('req', req.body);
-    con.query('SELECT * FROM users WHERE email = "' + req.body.email + '"', async function (err, result_data) {
+    const email=req.body.email;
+    const sql="SELECT * FROM users WHERE email ='"+email+"'";
+    con.query(sql, async function (err, result_data) {
         // console.log(result_data);
         if (err) {
             console.log(err);
@@ -42,7 +45,10 @@ postRegister = async function (req, res) {
 }
 postLogin = function (req, res) {
     // console.log('req', req.body);
-    con.query('SELECT * FROM users WHERE email = "' + req.body.email + '"', async function (err, result_data) {
+    const email = req.body.email;
+    const sql ="SELECT * FROM users WHERE email ='"+email+"'";
+    
+    con.query(sql, async function (err, result_data) {
         // console.log(result_data, req.body.password, result_data[0]['password']);
         if (err) {
             console.log(err);
@@ -72,8 +78,9 @@ postLogin = function (req, res) {
     // }
 }
 getViewProfile = function (req, res) {
-    // console.log('req', req.body);
-    let sql =   'SELECT * FROM users WHERE id = ';
+    // console.log('req', req.body.id);
+    const id=req.body.id;
+    let sql =   'SELECT * FROM users WHERE id ='+id;
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Result: " + JSON.stringify(result));
@@ -81,4 +88,57 @@ getViewProfile = function (req, res) {
     });
     
 }
-module.exports = { postRegister, getUserList, postLogin,getViewProfile }
+postUpdateProfile = function (req, res){
+    const id=req.body.id;
+    const first_name=req.body.first_name;
+    const last_name=req.body.last_name;
+    const address=req.body.address;
+    const email=req.body.email;
+    const city=req.body.city;
+    const state=req.body.state;
+    const country=req.body.country;
+    const zip_code=req.body.zip_code;
+    const mobile=req.body.mobile;
+    const password=req.body.password;
+    const sql= "UPDATE users SET first_name ='"+first_name+"', last_name = '"+ last_name +"', address = '"+ address +"', email = '"+ email +"', city = '"+ city+"', state = '"+ state +"', country = '"+ country +"', Zip_code = '"+ zip_code +"' , mobile = '"+ mobile +"', password = '"+ password +"'WHERE id ='"+id+"'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + JSON.stringify(result));
+        res.json(result);
+    });
+}
+postForgetPassword=function(req,res){
+    const password=req.body.password;
+    const email=req.body.email;
+    const sql="UPDATE users SET password ='"+password+" WHERE email ='"+email+"'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + JSON.stringify(result));
+        res.json(result);
+    });
+}
+postSetPassword=function(req,res){
+    const password=req.body.password;
+    const email=req.body.email;
+    const id=req.body.id;
+    const sql='UPDATE users SET password ='+password+', email= '+ email+'WHERE id ='+id;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + JSON.stringify(result));
+        res.json(result);
+    }); 
+}
+postReSetPassword=function(req,res){
+    const password=req.body.password;
+    const cpassword=req.body.cpassword;
+    const email=req.body.email;
+    const id=req.body.id;
+    const sql='UPDATE users SET password ='+password+', email= '+ email+'WHERE id ='+id;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + JSON.stringify(result));
+        res.json(result);
+    }); 
+
+}
+module.exports = { postRegister, getUserList, postLogin,getViewProfile ,postUpdateProfile,postForgetPassword,postSetPassword,postReSetPassword}
